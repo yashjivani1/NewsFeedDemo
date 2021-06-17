66ec5d857c77210701aa6357188ae3249e80ed3b
Yash Jivani <yash.jivani@iblinfotech.com>
2021-06-17 18:41:43 +0530

    Initial Commit

---
+++ NewsFeed/NewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Yash Jivani on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
